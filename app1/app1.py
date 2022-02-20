eec38faadf04936622c8aeb8bc4520fe6159125b
mrutkowsky <94920842+mrutkowsky@users.noreply.github.com>
2022-02-20 21:56:35 +0100

    added all files without the main folder

---
+++ app1/app1.py
@@ -0,0 +1,28 @@
+import time
+import requests
+import pandas as pd
+import json
+
+i = 0
+df = pd.read_csv("termometr1.csv", sep=",")
+
+
+def HTTP(url, frequency):
+    global i
+    json_data = json.loads(df.iloc[i].to_json())
+    print(json_data)
+    requests.post(url, json=json_data)
+    time.sleep(frequency)
+
+
+for i in range(len(df)):
+    app_nr = "app1"
+    with open('config1.json', 'r') as f:
+        config = json.load(f)
+        status = config['s_1']
+        frequency = int(config['fq'])
+        url = config['url']
+    if str(status) == str(1):
+        HTTP(url, frequency)
+    elif str(status) == str(0):
+        time.sleep(0.5)
